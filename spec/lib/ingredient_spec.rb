424b59be7c609aa2c80e796c287fe14c257a1e9e
Roger <justnyeh@gmail.com>
2014-07-22 23:19:02 +0100

    First test for ingredient spec

--- spec/lib/ingredient_spec.rb
+++ spec/lib/ingredient_spec.rb
@@ -2,4 +2,8 @@ require "spec_helper"
 
 describe Ingredient  do
 
+	subject{ Ingredient.new("sugar, spice and everything nice") }
+
+	it { expect(subject.full_name).to eq("sugar, spice and everything nice")  }
+
 end
